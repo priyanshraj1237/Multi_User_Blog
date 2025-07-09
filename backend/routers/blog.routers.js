ff5cb36875e42a933e9a48b1d1cbbbb521a90013
Priyanshu Ranjan <priyanshraj087@gmail.com>
2025-07-09 17:15:32 +0530

    updated the router api and checked the api

--- backend/routers/blog.routers.js
+++ backend/routers/blog.routers.js
@@ -7,7 +7,7 @@ const controllers = require('../controllers/blog.controllers.js');
 router.post('/create', verifyToken, controllers.createBlog);
 router.get('/retrive', verifyToken, controllers.getUserBlogs);
 router.get('/:id',verifyToken,controllers.getBlogbyId)
-router.delete('/json/:id',verifyToken,controllers.deleteblog)
-router.put('/update/:id',verifyToken,controllers.updatablog)
+router.delete('/:id',verifyToken,controllers.deleteblog)
+router.put('/:id',verifyToken,controllers.updatablog)
 
 module.exports = router;
